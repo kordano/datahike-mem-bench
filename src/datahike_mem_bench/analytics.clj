b78d9ee234fd3a2b1563859436e29ddc9aa8eb7c
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2021-12-22 15:06:00 +0100

    add analytics

--- src/datahike_mem_bench/analytics.clj
+++ src/datahike_mem_bench/analytics.clj
@@ -12,6 +12,7 @@
   (def tx-stats
     (let [metrics (->> (s/split (slurp "bench.log") #"\n")
                        (drop 5)
+                       (butlast)
                        (map #(s/split % #" "))
                        (map #(vector (-> % first instant/read-instant-date .getTime) (last %))))
           start-time (ffirst metrics)]
@@ -28,6 +29,30 @@
            (map (fn [m] (zipmap keyz (map #(Float/parseFloat %) m))))
            (map #(select-keys % [:Timestamp :OU])))))
 
+
+  (def jdbc-tx-stats
+    (let [metrics (->> (s/split (slurp "jdbc_bench.log") #"\n")
+                       (drop 11)
+                       (map #(s/split % #" "))
+                       (map #(vector (-> % first instant/read-instant-date .getTime) (last %))))
+          start-time (ffirst metrics)]
+      (->> metrics
+           (map (fn [[timestamp tx-count]] {:Timestamp (float (/ (- timestamp start-time) 1000))
+                                            :TxCount (Float/parseFloat tx-count)})))))
+
+  (count jdbc-tx-stats)
+
+  (def jdbc-jvm-stats
+    (let [[attrs & metrics] (->> (s/split (slurp "jdbc_jstat.out") #"\n")
+                                 (map #(s/split % #" "))
+                                 (map #(remove s/blank? %)))
+          keyz              (mapv keyword attrs)]
+      (->> metrics
+           (map (fn [m] (zipmap keyz (map #(Float/parseFloat %) m))))
+           (map #(select-keys % [:Timestamp :OU])))))
+
+  (count jdbc-jvm-stats)
+
   (count tx-stats)
 
   (count jvm-stats)
@@ -36,8 +61,8 @@
 
   (def combined-plot
     {:data {:values (vec (sort-by :Timestamp (concat tx-stats jvm-stats)))}
-     :width 2048
-     :height 1024
+     :width 1500
+     :height 1000
      :layer [{:mark {:stroke "red"
                      :type "line"
                      :interpolate "monotone"}
@@ -57,10 +82,36 @@
                               :title "Transaction count"}}}]
      :resolve {:scale {:y "independent"}}})
 
+  (def jdbc-combined-plot
+    {:data {:values (vec (sort-by :Timestamp (concat jdbc-tx-stats jdbc-jvm-stats)))}
+     :width 1500
+     :height 1000
+     :layer [{:mark {:stroke "red"
+                     :type "line"
+                     :interpolate "monotone"}
+              :encoding {:y {:field "OU"
+                             :type "quantitative"
+                             :title "Old space utilization"}
+                         :x {:field "Timestamp"
+                             :title "Seconds since start"
+                             :type "quantitative"}}}
+             {:mark {:type "circle"
+                     :color "blue"}
+              :encoding {:x {:field "Timestamp"
+                             :title "Seconds since start"
+                             :type "quantitative"}
+                         :y {:field "TxCount"
+                             :type "quantitative"
+                             :title "Transaction count"}}}]
+     :resolve {:scale {:y "independent"}}})
+
   (def viz
     [:div
      [:h2 "Datahike Memory Benchmark"]
-     [:vega-lite combined-plot]])
+     [:vega-lite combined-plot]
+     [:h2 "Datahike PG Memory Benchmark"]
+     [:vega-lite jdbc-combined-plot]
+     ])
 
   (oz/view! viz)
 
