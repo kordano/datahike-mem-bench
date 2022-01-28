f43baa14e8fd7f52849a1908aa9663f19bdf6b08
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2022-01-28 15:12:59 +0100

    add upsert and text to run script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clj -J-XX:+UseG1GC -J-XX:InitialHeapSize=2g -J-XX:MaxHeapSize=4g -J-XX:+UseStringDeduplication -J-XX:MaxTenuringThreshold=1 -J-Xlog:gc=debug:file=/root/datahike-mem-bench/gc.log:time,uptime,level,tags:filecount=5,filesize=100m -m datahike-mem-bench.core -c /root/datahike-mem-bench/config.edn -t 1000000 -l /root/datahike-mem-bench/log.out
+clj -J-XX:+UseG1GC -J-XX:InitialHeapSize=2g -J-XX:MaxHeapSize=4g -J-XX:+UseStringDeduplication -J-XX:MaxTenuringThreshold=1 -J-Xlog:gc=debug:file=/root/datahike-mem-bench/gc.log:time,uptime,level,tags:filecount=5,filesize=100m -m datahike-mem-bench.core -c /root/datahike-mem-bench/config.edn -t 1000000 -l /root/datahike-mem-bench/log.out -x 20 -u 100
